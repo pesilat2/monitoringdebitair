c3b43b1162e8e616055e06b23069064f2454a3ed
salmandma <salmandwimaulanaakbar96@gmail.com>
2023-07-20 13:53:00 +0700

    refactor: :recycle: refactor set up fe

---
+++ frontend/prettier.config.js
@@ -0,0 +1,8 @@
+// prettier.config.js
+
+module.exports = {
+  // Pengaturan Prettier lainnya
+  // ...
+  plugins: ["prettier-plugin-tailwindcss"], // Tambahkan plugin prettier-plugin-tailwindcss
+  vueIndentScriptAndStyle: true, // Aktifkan fitur untuk mengenali sintaksis Tailwind CSS dalam blok script dan style di file Vue
+};
