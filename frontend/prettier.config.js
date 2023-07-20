572127cf3aa87d31139611dec636f75e54d662f3
gelarahadian <gelarahadian38@gmail.com>
2023-07-20 20:13:58 +0700

    re setup nuxtjs tailwindcss

--- frontend/prettier.config.js
+++
@@ -1,8 +0,0 @@
-// prettier.config.js
-
-module.exports = {
-  // Pengaturan Prettier lainnya
-  // ...
-  plugins: ["prettier-plugin-tailwindcss"], // Tambahkan plugin prettier-plugin-tailwindcss
-  vueIndentScriptAndStyle: true, // Aktifkan fitur untuk mengenali sintaksis Tailwind CSS dalam blok script dan style di file Vue
-};
