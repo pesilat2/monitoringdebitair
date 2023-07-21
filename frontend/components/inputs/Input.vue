61eed7e08bdc4cd5e9df760a0358aee6290999f5
gelarahadian <gelarahadian38@gmail.com>
2023-07-21 13:28:46 +0700

    feature: menambahkan select pada form register

--- frontend/components/inputs/Input.vue
+++ frontend/components/inputs/Input.vue
@@ -4,12 +4,19 @@
       >{{ label }}:</label
     >
     <div
-      class="flex gap-5 h-12 items-center w-full border border-[#7895CB] rounded px-5"
+      class="flex gap-5 h-12 items-center w-full border border-[#7895CB] rounded px-5 hover:shadow-lg shadow-[#7895CB] transition-colors"
     >
       <div class="text-2xl text-[#7895CB]">
         <i :class="icon"></i>
       </div>
-      <input :type="type" :id="id" class="w-full h-full outline-none" />
+      <input
+        :type="type"
+        :id="id"
+        :autofocus="autoFocus"
+        class="w-full h-full outline-none text-2xl text-[#7895CB]"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+      />
     </div>
   </div>
 </template>
@@ -22,6 +29,8 @@ export default {
     id: String,
     type: String,
     icon: String,
+    value: String,
+    autoFocus: Boolean,
   },
 };
 </script>
---
+++ frontend/components/inputs/Select.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <label class="text-3xl font-normal text-[#7895CB]" :for="id"
+      >{{ label }}:</label
+    >
+    <div
+      class="border text-xl border-[#7895CB] rounded px-5 hover:shadow-lg shadow-[#7895CB] transition-colors"
+    >
+      <select
+        :value="value"
+        @change="$emit('input', $event.target.value)"
+        :id="id"
+        class="w-full h-12 outline-none text-[#7895CB]"
+      >
+        <option
+          class="text-xl border border-[#7895CB] font-normal text-[#7895CB] mb-2"
+          v-for="city in data"
+          :key="city"
+        >
+          {{ city }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    data: Array,
+    id: String,
+    value: String,
+  },
+};
+</script>
